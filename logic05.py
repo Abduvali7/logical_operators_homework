54987ae950968a65a14d19bb92bd7c203e5827c3
Abduvali7 <hamzaqulovagulmira@gmail.com>
2023-03-02 11:17:34 +0500

    05

--- logic05.py
+++ logic05.py
@@ -1,10 +1,11 @@
 def main(a,b):
     """
-    Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is odd".
+    Given two integers a, b,  check the following statement "Each of the numbers 'a' or 'b' is even".
     Args:
         a(int): parameter a
         b(int): parameter b
     Returns:
         bool: answer
     """
-    return
+    return a%2==0 or b%2==0
+print(main(2,4))
